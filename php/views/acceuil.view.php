f602e8e6cad932c14a1200fc5332fffa456b356e
Luc <luc.francois.1994@gmail.1994>
2018-11-16 09:20:10 +0100

    2nd COMMIT BITCH

--- php/views/acceuil.view.php
+++ php/views/acceuil.view.php
@@ -1,11 +1,47 @@
-<div class="container">
-    <img src="../ressources/SelfHeroes.png">
-    <div class="col-md-10 col-md-offset-1">
-
-        <h1><?=$title?></h1>
-        <p><?=$text?></p>
-
-        <a href="/SelfHeroes/php/movie/addUpdate"><button>Sign Up</button></a>
-        <a href="/SelfHeroes/php/movie/delete"><button>Sign In</button></a>
-    </div>
+<div id="sidebar">
+    <ul class="menu">
+        <li class="menu-global menu-top" onclick="window.open('https://codepen.io/woodwork/', '_blank');">
+            <span class="menu-text">More Pens</span>
+        </li>
+        <li class="menu-global menu-middle">
+            <span class="menu-text">Ai Weiwei</span>
+        </li>
+        <li class="menu-global menu-bottom">
+            <span class="menu-text">Bert Heller</span>
+        </li>
+        <li class="menu-global menu-hidden">
+            <span class="menu-text">Gustav Klimt</span>
+        </li>
+        <li class="menu-global menu-hidden">
+            <span class="menu-text">Paul Klee</span>
+        </li>
+        <li class="menu-global menu-hidden">
+            <span class="menu-text">Egon Schiele</span>
+        </li>
+        <div class="menu-close fa fa-close"></div>
+        <div class="click-blocker"></div>
+    </ul>
 </div>
+
+<div id="page">
+    <h1>The same bones as <a href="https://codepen.io/woodwork/pen/BoEGYZ" target="_blank">Version 1...</a></h1>
+    <h2>But now with the posibility to have more than three menu items.</h2>
+    <p>Currently limited to 10 items. If you need even more add extra colors to the SCSS <code>$menuColor</code> list and make sure your added <code>&lt;li&gt;</code> items classes are set to <code>"menu-global menu-hidden"</code></p>
+</div>
+
+
+
+<!--<div id="sign" class="flex items-center justify-between fixed w-full bg-white">
+            <img src="../ressources/logo.png" alt="" class="self-start ml-5 " id="logo">
+            <div class="flex justify-around">
+                <a href="/SelfHeroes/php/movie/addUpdate"><button class="mr-5 bg-blue hover:bg-blue-light text-white font-bold py-2 px-4 border-b-4 border-blue-dark hover:border-blue rounded">Sign Up</button></a>
+                <a href="/SelfHeroes/php/movie/delete"><button class="mr-5 bg-blue hover:bg-blue-light text-white font-bold py-2 px-4 border-b-4 border-blue-dark hover:border-blue rounded">Sign In</button></a>
+            </div>
+
+        </div>
+        <div id="TopAcceuil" class="h-full">
+        </div>
+        <div id="MidAcceuil" class="h-full">
+            <p></p>
+        </div>
+    </div>-->
--- php/views/layout.view.php
+++ php/views/layout.view.php
@@ -1,7 +1,8 @@
 <html>
     <head>
         <title>Films</title>
-<!--        <link rel="stylesheet" href="css/bootstrap.min.css">-->
+        <link href="https://cdn.jsdelivr.net/npm/tailwindcss/dist/tailwind.min.css" rel="stylesheet">
+        <!--<link rel="stylesheet" href="css/bootstrap.min.css">-->
         <!--<link rel="stylesheet" type="text/css" href="../css/style.css">
         <link rel="stylesheet" type="text/css" href="../../css/style.css">-->
         <?php
@@ -10,10 +11,17 @@
                 $pageno=strstr($_SERVER['REQUEST_URI'], "pageno");
             }
 
+
+
             if ($_SERVER['REQUEST_URI']=="/SelfHeroes/php/" || $pageno != "") {
                 echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"../css/style.css\">";
+                echo "<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js'></script>";
+                echo "<script src='../js/menu.js'></script>";
+
             }else if ($_SERVER['REQUEST_URI']=="/SelfHeroes/php/movie/addUpdate" || $_SERVER['REQUEST_URI']=="/SelfHeroes/php/movie/delete") {
                 echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"../../css/style.css\">";
+                echo "<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js'></script>";
+                echo "<script src='../../js/menu.js'></script>";
             }
         ?>
     </head>
