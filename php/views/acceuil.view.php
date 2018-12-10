5cfa73c7c722dccc3808092ebbad563cb1708922
Luc <luc.francois.1994@gmail.1994>
2018-12-10 10:49:23 +0100

    Css page d'acceuil

--- php/views/acceuil.view.php
+++ php/views/acceuil.view.php
@@ -1,18 +1,20 @@
-<div class="container">
-    <img src="../ressources/SelfHeroes.png">
-    <div class="col-md-10 col-md-offset-1">
 
-        <h1><?=$title?></h1>
-        <p><?=$text?></p>
-
-        <a href="/SelfHeroes/php/signUp"><button>Sign Up</button></a>
-        <a href="/SelfHeroes/php/signIn"><button>Sign In</button></a>
+<div id="sign" class="flex items-center justify-between fixed w-full bg-white">
+    <img src="../ressources/logo.png" alt="" class="self-start ml-5 " id="logo">
+    <div class="flex justify-around">
+        <a href="/SelfHeroes/php/signUp"><button class="mr-5 bg-blue hover:bg-blue-light text-white font-bold py-2 px-4 border-b-4 border-blue-dark hover:border-blue rounded">Sign Up</button></a>
+        <a href="/SelfHeroes/php/signIn"><button class="mr-5 bg-blue hover:bg-blue-light text-white font-bold py-2 px-4 border-b-4 border-blue-dark hover:border-blue rounded">Sign In</button></a>
     </div>
+</div>
+
+<div class="">
+    <img src="../ressources/SelfHeroes.png" class="w-full">
+    <img src="../ressources/livre-ouvert.jpg" class="w-full">
 </div>
 
 
 
-<!--<div id="sign" class="flex items-center justify-between fixed w-full bg-white">
+<!--    <div id="sign" class="flex items-center justify-between fixed w-full bg-white">
             <img src="../ressources/logo.png" alt="" class="self-start ml-5 " id="logo">
             <div class="flex justify-around">
                 <a href="/SelfHeroes/php/movie/addUpdate"><button class="mr-5 bg-blue hover:bg-blue-light text-white font-bold py-2 px-4 border-b-4 border-blue-dark hover:border-blue rounded">Sign Up</button></a>
